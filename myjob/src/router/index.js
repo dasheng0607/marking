6bdba97f7980646519854648e97057d2f05d730b
lds <sanda0607@163.com>
2018-09-08 12:05:20 +0800

    updata

--- myjob/src/router/index.js
+++ myjob/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import index from '@/components/index'
+import HelloWorld from '@/views/HelloWorld'
+import index from '@/views/index'
 
 Vue.use(Router)
 
