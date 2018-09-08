f4cd4c42760a79bac02e3536bfd883776c91e6ea
Hung <610499659@qq.com>
2018-09-08 11:10:47 +0800

    修改配置

--- myjob/build/webpack.base.conf.js
+++ myjob/build/webpack.base.conf.js
@@ -3,14 +3,14 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const vuxLoader = require('vux-loader')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
 
-
-module.exports = {
+const webpackConfig = {
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
@@ -23,10 +23,12 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.less'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'components': resolve('src/components'),
+      'views': resolve('src/views'),
     }
   },
   module: {
@@ -80,3 +82,5 @@ module.exports = {
     child_process: 'empty'
   }
 }
+
+module.exports = vuxLoader.merge(webpackConfig, {plugins: ['vux-ui']})
