4492abd6397585bfe53aa78287933b2e318758f3
lds <sanda0607@163.com>
2018-09-09 23:46:05 +0800

    7星页面部分

--- myjob/src/router/index.js
+++ myjob/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/views/HelloWorld'
 import index from '@/views/index'
+import getstart from '@/views/getstart'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/index',
       name: 'index',
       component: index
+    },
+    {
+      path: '/getstart',
+      name: 'getstart',
+      component: getstart
     }
   ]
 })
