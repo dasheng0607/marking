fa7c7e882abf81c244552a9ca993cc46a05758f4
v_dshnlian <v_dshnlian@tencent.com>
2018-09-11 20:07:41 +0800

    公司最新数据updata

--- myjob/config/index.js
+++ myjob/config/index.js
@@ -11,8 +11,8 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api/*': {
-        target: 'http://localhost:8989',
+      '/qxby/*': {
+        target: 'https://java.howjoin.com',
         secure: false
       }
     },
